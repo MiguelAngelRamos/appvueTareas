1b8e3ad2ce0ae08f4f3c799e485a7981b2de679d
Miguel Angel Ramos <return.infox@gmail.com>
2022-03-03 20:04:37 -0300

    config tareas

---
+++ src/components/Tarea.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-8 offset-2">
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Ingrese una tarea"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import VueSweetalert2 from 'vue-sweetalert2';
 
-createApp(App).mount('#app')
+createApp(App).use(VueSweetalert2).mount('#app')
