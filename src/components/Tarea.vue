c65de4df1e2fc75523cb1868db2955f2184922ad
Miguel Angel Ramos <return.infox@gmail.com>
2022-03-04 19:59:37 -0300

    repositorio actualizado

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -11,33 +11,42 @@
         />
       </div>
 
-      <div class="col-6 offset-3 mt-2" >
-
+      <div class="col-6 offset-3 mt-2">
         <div v-if="tareas.length === 0" class="card p-2 alert alert-warning">
-          <h6 class="text-center" >No hay tareas para mostrar...</h6>
+          <h6 class="text-center">No hay tareas para mostrar...</h6>
         </div>
 
         <!-- Aqui vamos a renderizar nuestras tareas -->
-        <ul class="list-group" >
-          <li v-for="(tarea, index) of tareas" :key="index" class="list-group-item d-flex align-items-center justify-content-between">
+        <ul class="list-group">
+          <li
+            v-for="(tarea, index) of tareas"
+            :key="index"
+            class="
+              list-group-item
+              d-flex
+              align-items-center
+              justify-content-between
+            "
+          >
             <span class="cursor" @click="actualizarTarea(tarea, index)">
-
-              <i :class="tarea.estado?'fas fa-check-circle text-success':'far fa-circle'"></i>
+              <i
+                :class="
+                  tarea.estado
+                    ? 'fas fa-check-circle text-success'
+                    : 'far fa-circle'
+                "
+              ></i>
               <!-- <i :class='tarea.estado?"fas fa-check-circle text-success":"far fa-circle"'></i> -->
             </span>
-            
+
             <h5>{{ tarea.nombre }}</h5>
 
             <span class="cursor text-danger" @click="eliminarTarea(index)">
               <i class="fa fa-trash-alt"></i>
             </span>
-
           </li>
         </ul>
-        
       </div>
-
-
     </div>
   </div>
 </template>
@@ -47,22 +56,20 @@ export default {
   data() {
     return {
       tareas: [],
-      nombreTarea: ""
-    }
+      nombreTarea: "",
+    };
   },
   methods: {
     agregarTarea() {
-
-      if(this.nombreTarea === "") {
-
+      if (this.nombreTarea === "") {
         this.$swal.fire({
-          title: 'Debes crear una tarea!!!',
+          title: "Debes crear una tarea!!!",
           showClass: {
-            popup: 'animate__animated animate__fadeInDown'
+            popup: "animate__animated animate__fadeInDown",
           },
-          hideClass:{ 
-            popup: 'animate__animated animate__fadeOutUp'
-          }
+          hideClass: {
+            popup: "animate__animated animate__fadeOutUp",
+          },
         });
         return;
       }
@@ -71,30 +78,53 @@ export default {
       // Nuestra tarea
       const tareaObject = {
         nombre: this.nombreTarea,
-        estado: false
-      }
+        estado: false,
+      };
 
       this.nombreTarea = "";
       // console.log(tareaObject)
       this.tareas.push(tareaObject);
-      console.log(this.tareas)
+      console.log(this.tareas);
+      localStorage.setItem("tareas-storage", JSON.stringify(this.tareas));
+      
     },
     eliminarTarea(indice) {
-      // tareas['angular', 'react', 'vue']
-      // indice indicamos donde queremos empezar a eliminar
-      // con el 1 indicamos cuantos elementos deseamos eliminar
-       this.tareas.splice(indice, 1);
+      this.$swal.fire({
+          title: "Estas seguro?",
+          text: "No podrás revertir esto!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#30d6a1",
+          cancelButtonColor: "",
+          confirmButtonText: "Si, Eliminar tarea!",
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            // tareas['angular', 'react', 'vue']
+            // indice indicamos donde queremos empezar a eliminar
+            // con el 1 indicamos cuantos elementos deseamos eliminar
+            this.tareas.splice(indice, 1);
+            Swal.fire("Eliminada!", "Tu tarea ha sido eliminada.", "success");
+          }
+        });
     },
     actualizarTarea(tarea, indice) {
       this.tareas[indice].estado = !tarea.estado; // tarea.estado esta en false
-      console.log(this.tareas)
+      console.log(this.tareas);
+    },
+  },
+  created() {
+    // llamar la información que existe dentro del local storage
+    if(localStorage.getItem("tareas-storage")) {
+      console.log("EXISTE INFORMACION EN EL LOCAL STORAGE");
+      this.tareas = JSON.parse(localStorage.getItem("tareas-storage"));
     }
   }
 };
 </script>
 
 <style>
-  .cursor {
-    cursor: pointer;
-  }
+.cursor {
+  cursor: pointer;
+}
 </style>
