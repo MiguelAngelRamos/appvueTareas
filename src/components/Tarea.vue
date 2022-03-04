4dda0fec91c226e4f4d33c4f68ec76cede71e90e
Miguel Angel Ramos <return.infox@gmail.com>
2022-03-03 22:30:42 -0300

    tareas

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -12,15 +12,29 @@
       </div>
 
       <div class="col-6 offset-3 mt-2" >
-        <div class="card p-2">
+
+        <div v-if="tareas.length === 0" class="card p-2 alert alert-warning">
           <h6 class="text-center" >No hay tareas para mostrar...</h6>
         </div>
+
         <!-- Aqui vamos a renderizar nuestras tareas -->
         <ul class="list-group" >
-          <li class="list-group-item">
-            <h5>Proyecto de Vue</h5>
+          <li v-for="(tarea, index) of tareas" :key="index" class="list-group-item d-flex align-items-center justify-content-between">
+            <span class="cursor" @click="actualizarTarea(tarea, index)">
+
+              <i :class="tarea.estado?'fas fa-check-circle text-success':'far fa-circle'"></i>
+              <!-- <i :class='tarea.estado?"fas fa-check-circle text-success":"far fa-circle"'></i> -->
+            </span>
+            
+            <h5>{{ tarea.nombre }}</h5>
+
+            <span class="cursor text-danger" @click="eliminarTarea(index)">
+              <i class="fa fa-trash-alt"></i>
+            </span>
+
           </li>
         </ul>
+        
       </div>
 
 
@@ -38,6 +52,20 @@ export default {
   },
   methods: {
     agregarTarea() {
+
+      if(this.nombreTarea === "") {
+
+        this.$swal.fire({
+          title: 'Debes crear una tarea!!!',
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass:{ 
+            popup: 'animate__animated animate__fadeOutUp'
+          }
+        });
+        return;
+      }
       // console.log('AGREGANDO UNA TAREA....');
 
       // Nuestra tarea
@@ -45,8 +73,20 @@ export default {
         nombre: this.nombreTarea,
         estado: false
       }
+
+      this.nombreTarea = "";
       // console.log(tareaObject)
       this.tareas.push(tareaObject);
+      console.log(this.tareas)
+    },
+    eliminarTarea(indice) {
+      // tareas['angular', 'react', 'vue']
+      // indice indicamos donde queremos empezar a eliminar
+      // con el 1 indicamos cuantos elementos deseamos eliminar
+       this.tareas.splice(indice, 1);
+    },
+    actualizarTarea(tarea, indice) {
+      this.tareas[indice].estado = !tarea.estado; // tarea.estado esta en false
       console.log(this.tareas)
     }
   }
@@ -54,4 +94,7 @@ export default {
 </script>
 
 <style>
+  .cursor {
+    cursor: pointer;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 createApp(App).use(VueSweetalert2).mount('#app')
